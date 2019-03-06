24c76e29d9798543d5b386d69305a53a1aa75418
MeganathanR <46572574+MeganathanR@users.noreply.github.com>
2019-03-06 16:20:12 +0530

    Update case37.py

--- case37.py
+++ case37.py
@@ -1,7 +1,8 @@
-num1 = input(' ')
-num2 = input(' ')
+x,y = input().split( )
+num1=int(x)
+num2=int(y)
 temp = num1
 num1 = num2
 num2 = temp
-print("",num1)
-print("", num2)
+print(num1,end=" ")
+print(num2)
