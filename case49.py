66e58cab24090e03690fa6774aa726c0e2dc6355
MeganathanR <46572574+MeganathanR@users.noreply.github.com>
2019-03-06 17:57:25 +0530

    Update case49.py

--- case49.py
+++ case49.py
@@ -1,3 +1,6 @@
+x,y=input().split( )
+n=int(x)
+m=int(y)
 n=int(input()) #enter the first value
 m=int(input()) #enter the second value
-print(n+m) #yhe sum of two numbers
+print(n+m) #the sum of two numbers
